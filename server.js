34767571e1e7d0d3cd2c3413170e10399ef0d833
Daksh Budhiraja <dakshbudhiraja@MacBook-Air.local>
2021-06-07 19:48:50 +0530

    3

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const path = require('path');
 const cors = require("cors");
 const https = require("https");
 
 const app =express();
 app.use(cors());
 
-
+app.use('/', express.static(path.join(__dirname, './client/build')));
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
   extended: true
@@ -19,8 +19,7 @@ const mainRouter = require("./route/main");
 app.use("",mainRouter)
 
 if ( process.env.NODE_ENV == "production"){
-    app.use(express.static("client/build"));
-    const path = require("path");
+    
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
